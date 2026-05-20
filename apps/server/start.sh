f21ab097e625cec226a2b888e09b1d55acb86331
luis <luis.enriquez@sonorasoft.com>
2026-05-20 16:39:32 -0700

    deploy changes v3

--- apps/server/start.sh
+++ apps/server/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
-npx prisma migrate deploy --schema=../../packages/db/prisma/schema.prisma
-npx prisma migrate deploy --schema=../../packages/db/prisma/auth.prisma
-exec node dist/index.ts
+npx prisma migrate deploy --schema=./packages/db/prisma/schema.prisma
+npx prisma migrate deploy --schema=./packages/db/prisma/auth.prisma
+exec node apps/server/dist/index.js
