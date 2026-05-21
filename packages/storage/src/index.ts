4a0a60f70a94ee0c49bdf6d742346697f5b77556
luis <luis.enriquez@sonorasoft.com>
2026-05-21 09:24:58 -0700

    deploy v17

--- packages/storage/src/index.ts
+++ packages/storage/src/index.ts
@@ -1,2 +1,5 @@
 export { storageService, createStorageService } from "./factory";
 export type { StorageService, UploadResult } from "./types";
+
+export * from "./factory.ts";
+export * from "./types.ts";
